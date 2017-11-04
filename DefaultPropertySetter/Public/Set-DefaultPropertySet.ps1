f6c4965fc15d9e466153238a8c5677cac5d7563d
Windos <windosbk+github@gmail.com>
2017-11-04 20:41:48 +1300

    Start of set function

--- DefaultPropertySetter/Public/Set-DefaultPropertySet.ps1
+++ DefaultPropertySetter/Public/Set-DefaultPropertySet.ps1
@@ -1,1 +1,40 @@
 # Replaces data on an existing resource or creates a resource that contains some data.
+
+function Set-DefaultPropertySet {
+    [CmdletBinding()]
+    Param (
+        # Parameter help description
+        [Parameter(Mandatory,
+                   ValueFromPipeline,
+                   ValueFromPipelineByPropertyName)]
+        [PSCustomObject[]] $Object,
+
+        # Parameter help description
+        [Parameter(Mandatory)]
+        [String[]] $DisplaySet,
+
+        [Switch] $Force
+    )
+        $DefaultDisplaySet = $DisplaySet
+        $DefaultDisplayPropertySet = New-Object System.Management.Automation.PSPropertySet(‘DefaultDisplayPropertySet’,[string[]]$DefaultDisplaySet)
+        $PSStandardMembers = [System.Management.Automation.PSMemberInfo[]]@($DefaultDisplayPropertySet)
+    }
+
+    Process {
+        foreach ($Obj in $Object) {
+            if ($Force) {
+                $Obj | Add-Member MemberSet PSStandardMembers $PSStandardMembers -Force
+            } else {
+                try {
+                    $Obj | Add-Member MemberSet PSStandardMembers $PSStandardMembers -ErrorAction Stop
+                } catch [InvalidOperationException] {
+                    Write-Error -Exception 'Cannot set a Default Property Set on () because a Default Property Set already exists. To overwrite the Property Set, add the Force parameter to your command.'
+                    Continue
+                }
+            }
+            $Obj
+        }
+    }
+
+    End {}
+}
