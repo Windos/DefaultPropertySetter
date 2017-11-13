23717eff7ffc245b6fbbaeda2b9f6b390296239d
Windos <windosbk+github@gmail.com>
2017-11-14 05:07:57 +1300

    this too!

--- DefaultPropertySetter/Public/Get-DefaultPropertySet.ps1
+++ DefaultPropertySetter/Public/Get-DefaultPropertySet.ps1
@@ -1,1 +1,23 @@
 # Specifies an action that retrieves a resource.
+
+function Get-DefaultPropertySet {
+    [CmdletBinding()]
+    param (
+        # comment based parameter help
+        [Parameter(Mandatory,
+                   ValueFromPipeline)]
+        [PSCustomObject[]] $Object
+    )
+
+    Begin {}
+
+    Process {
+        foreach ($Obj in $Object) {
+            [PSCustomObject] @{
+                DisplaySet = $Obj.PSStandardMembers.DefaultDisplayPropertySet.ReferencedPropertyNames
+            }
+        }
+    }
+
+    End {}
+}
