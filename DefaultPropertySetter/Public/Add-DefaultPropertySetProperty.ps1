47789c3656bf6cdd28c0cf6cbfdb2bf3c3bf96e8
Windos <windosbk+github@gmail.com>
2017-11-14 05:32:43 +1300

    Live stream wrap up

--- DefaultPropertySetter/Public/Add-DefaultPropertySetProperty.ps1
+++ DefaultPropertySetter/Public/Add-DefaultPropertySetProperty.ps1
@@ -1,1 +1,26 @@
 # $DemoObject1.PSStandardMembers.DefaultDisplayPropertySet.ReferencedPropertyNames.Add('Country')
+
+function Add-DefaultPropertySetProperty {
+    [CmdletBinding()]
+    param (
+        # comment based parameter help
+        [Parameter(Mandatory,
+                   ValueFromPipeline)]
+        [PSCustomObject[]] $Object,
+
+        [Parameter(Mandatory)]
+        [String[]] $Property
+    )
+
+    Begin {}
+
+    Process {
+        foreach ($Obj in $Object) {
+            foreach ($Prop in $Property) {
+                $Obj.PSStandardMembers.DefaultDisplayPropertySet.ReferencedPropertyNames.Add($Prop)
+            }
+        }
+    }
+
+    End {}
+}
--- DefaultPropertySetter/Public/Remove-DefaultPropertySetProperty.ps1
+++ DefaultPropertySetter/Public/Remove-DefaultPropertySetProperty.ps1
@@ -1,1 +1,26 @@
 # $DemoObject1.PSStandardMembers.DefaultDisplayPropertySet.ReferencedPropertyNames.Remove('Country')
+
+function Remove-DefaultPropertySetProperty {
+    [CmdletBinding()]
+    param (
+        # comment based parameter help
+        [Parameter(Mandatory,
+                   ValueFromPipeline)]
+        [PSCustomObject[]] $Object,
+
+        [Parameter(Mandatory)]
+        [String[]] $Property
+    )
+
+    Begin {}
+
+    Process {
+        foreach ($Obj in $Object) {
+            foreach ($Prop in $Property) {
+                $null = $Obj.PSStandardMembers.DefaultDisplayPropertySet.ReferencedPropertyNames.Remove($Prop)
+            }
+        }
+    }
+
+    End {}
+}
