ab37f99d117697240255b479a3e68c6925d52054
Windos <windosbk+github@gmail.com>
2017-11-25 16:06:45 +1300

    Dynamic parameter for displayset
    Using test code from last live stream

--- DefaultPropertySetter/Public/Set-DefaultPropertySet.ps1
+++ DefaultPropertySetter/Public/Set-DefaultPropertySet.ps1
@@ -1,5 +1,3 @@
-# Replaces data on an existing resource or creates a resource that contains some data.
-
 function Set-DefaultPropertySet {
     <#
     .SYNOPSIS
@@ -19,6 +17,8 @@ function Set-DefaultPropertySet {
     The name of the properties to be included with the Default Display Property
     Set.
 
+    Only paramater names that exist on the first input Object are valid.
+
     .PARAMETER Force
     Specifies that an existing Default Display Property Set can be overwritten.
 
@@ -35,11 +35,25 @@ function Set-DefaultPropertySet {
                    ValueFromPipeline)]
         [PSCustomObject[]] $Object,
 
-        [Parameter(Mandatory)]
-        [String[]] $DisplaySet,
-
         [Switch] $Force
     )
+
+    DynamicParam {
+        $ParamAttrib  = New-Object System.Management.Automation.ParameterAttribute
+        $ParamAttrib.Mandatory  = $true
+        $ParamAttrib.ParameterSetName  = '__AllParameterSets'
+        $AttribColl = New-Object  System.Collections.ObjectModel.Collection[System.Attribute]
+        $AttribColl.Add($ParamAttrib)
+
+        $AvailablePropertyNames = $MyInvocation.BoundParameters.Item('Object')[0] | Get-Member -MemberType Properties | Select -ExpandProperty Name
+        $AttribColl.Add((New-Object  System.Management.Automation.ValidateSetAttribute($AvailablePropertyNames)))
+
+        $RuntimeParam  = New-Object System.Management.Automation.RuntimeDefinedParameter('DisplaySet',  [string[]], $AttribColl)
+        $RuntimeParamDic  = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
+        $RuntimeParamDic.Add('DisplaySet',  $RuntimeParam)
+
+        return  $RuntimeParamDic
+    }
 
     Begin {
         $DefaultDisplaySet = $DisplaySet
